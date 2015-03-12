4d689560ff6987ad9e520a0a8efa056faaf4275e
dorienhsmns <dorienhuysmans@gmail.com>
2015-03-12 15:42:34 +0100

    ignore

---
+++ SensorData/config_room.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 14:42:09 2015
+
+@author: dorienhuysmans
+"""
+
+{ 
+ 	"i2cSensors": [ 
+# 		{ 
+# 			"name": "Air_Pressure", 
+# 			"type": "Pressure", 
+# 			"class": "MPL3115A2" 
+# 		}, 
+# 		{ 
+# 			"name": "Light_Sensor", 
+# 			"type": "Light", 
+# 			"class": "TSL2561" 
+# 		},
+#        { 
+# 			"name": "Temp_of_head", 
+# 			"type": "infra", 
+# 			"class": "MLX90614" 
+# 		},                
+#        { 
+# 			"name": "Temp_of_head2", 
+# 			"type": "infra", 
+# 			"class": "MLX90614" 
+# 		}, 
+#        { 
+# 			"name": "Table_temp", 
+# 			"type": "temp", 
+# 			"class": "MR003" 
+# 		}
+ 	], 
+ 	 
+ 	"AnalogSensors": [ 
+ 		{ 
+ 			"name": "sound_sensor", 
+ 			"type": "grove", 
+ 			"pin": "0" 
+ 		}
+    		{ 
+ 			"name": "temp_sensor", 
+ 			"type": "grove", 
+ 			"pin": "1" 
+ 		}
+    		{ 
+ 			"name": "light_sensor", 
+ 			"type": "grove", 
+ 			"pin": "2" 
+ 		}
+    		{ 
+ 			"name": "movement_sensor", 
+ 			"type": "grove", 
+ 			"pin": "3" 
+ 		}
+        
+	
+ 	],
+ 	
+ 	"DigitalSensors": [
+ 	]
+} 
